9a142982f20bc65292aac40ee298de1b31dec42f
NAMAN JAIN <namanjain152003@gmail.com>
2025-02-13 11:36:05 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,2 +1,13 @@
 const accountId = 144553
-let accountEmail="nana"
+let accountEmail="namanjain@gmail.com"
+var accountpassword="12345"
+accountcity="jaipur"
+// we can declare variables without var or let bu this is not a good approach ....
+//accountId=2 // not allowed to change
+/*prefer not to use var , because of the issue in block scope and functional scope*/
+accountEmail="hch.com"
+accountpassword="222222"
+accountcity="delhi"
+console.log(accountId)
+let accountState;
+console.table([accountId,accountEmail,accountpassword,accountcity,accountState])
