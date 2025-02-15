fefa4aec5224e6c33560f7d89821d6bc64d2f8d7
NAMAN JAIN <namanjain152003@gmail.com>
2025-02-15 07:55:18 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,4 +4,21 @@
 
 console.log(3+3);
 console.log("naman")
+// readability should be good 
 
+let name ="naman"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol ( used for uniqueness)
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
