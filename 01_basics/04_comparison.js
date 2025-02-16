1af1015e8efc4c7b6b9cfb3d16e586dfd588ec5f
NAMAN JAIN <namanjain152003@gmail.com>
2025-02-16 14:34:49 +0000

    comparisons

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -8,11 +8,17 @@ console.log(2 !=1 );*/
 console.log("2">1);
 console.log("02">1);
 
+
+// avoid the below conversions
 console.log(null>0);
 console.log(null==0);//
 console.log(null>=0);// here null is converted into 0
 
 console.log(undefined == 0);
+
+// === (strict check) it checks value as well as datatypes
+
+console.log("2"===2);
 
 
 
