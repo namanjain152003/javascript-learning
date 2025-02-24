92f297f537fb115a964002385c85fe5b159ca81a
NAMAN JAIN <namanjain152003@gmail.com>
2025-02-24 14:39:14 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,3 +1,25 @@
 // ARRAY
 const myarr =[0,1,2,3,4,5] 
 const myArr = [0,1,2,3, true ," hitesh"] // we can use different types of element in array also
+const myHeros =["shaktimaan", " bheem", "ironman"]
+// javascript arrays are resizable
+
+const myArr2= new Array(1,2,3,4)
+console.log(myArr[5]);
+console.log(myArr2);
+
+
+// ARRAY METHODS :
+
+myArr.push(6)    // push add the new element to the array
+myArr.push(7)
+myArr.pop()   // it removes the last element of the array 
+ 
+myArr.unshift(9) // it add the element at starting of the array
+myArr.shift()    // it removes the first element of the array
+
+
+console.log(myArr.includes(3));
+
+console.log(myArr);
+
