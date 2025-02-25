5e320b0bbad3617911ea6860190bf8d911a0fe7f
NAMAN JAIN <namanjain152003@gmail.com>
2025-02-25 18:59:49 +0000

    arrays part 2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -5,11 +5,11 @@ marvel_heros.push(DC) // here another array is become the new object of the exis
 console.log(marvel_heros[3][1]);
 
 
- concat operator
+ //concat operator
 const newArr = marvel_heros.concat(DC) //  this will properly join the two arrays and output should be taken in another array
 console.log(newArr); // whole 
 
-spread method
+//spread method
 const all_new_heros = [...marvel_heros, ...DC]
 console.log(all_new_heros);
 
