04ce5e8683237a57d54ea0d65bd12c1d98e84f36
NAMAN JAIN <namanjain152003@gmail.com>
2025-02-26 18:46:59 +0000

    object part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,43 @@
+//const tinderUser = new Object()  // this is singleton object
+
+const tinderUser ={}
+tinderUser.id="123abc"
+tinderUser.name="naman"
+tinderUser.isLoggedIn = false
+//console.log(tinderUser);
+const regularUser ={
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"naman",
+            lastname:"jain"
+        }
+    }
+
+} 
+// this is how we can use nested objects
+//console.log(regularUser.fullname.userfullname.firstname);
+
+
+const obj1 ={1:"a" , 2:"b"}
+const obj2 ={3:"a", 4:"b"}
+//const obj3 = {obj1 , obj2} // in this both objects will be in object
+//const obj3 = Object.assign({}, obj1,obj2) // this will clearly combine both objects in single object
+// here {} act as target and all objects act as source
+const obj3={...obj1,...obj2}
+console.log(obj3);
+
+
+const users = [
+    {
+        id:1,
+        email:"naman@google"
+    }
+]
+console.log(users[0].email)
+console.log(tinderUser);
+console.log(Object.keys(tinderUser)); // it returns keys in array
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); //helps to wheather the required entry is present in object or not
